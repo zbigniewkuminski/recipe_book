4005e7da8a0b56c04d48f24c80129a47423e0036
Zbigniew Kumiński <zbigniew.kuminski@gmail.com>
2018-07-05 10:05:22 +0200

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  password = 'masło';
-  passwordHidden = true;
-  justArray = [];
-  
-  btnClick() {
-    this.passwordHidden = !this.passwordHidden;
-    this.justArray.push(this.justArray.length + 1 +' ' + new Date());
-  }
+  title = '';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
