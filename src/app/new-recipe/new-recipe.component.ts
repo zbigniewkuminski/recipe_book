c4a4b4e4ae98a70159c9d99ed98f117ea646e551
Zbigniew Kumiński <zbigniew.kuminski@gmail.com>
2018-07-09 15:08:04 +0200

    cc

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-new-recipe',
@@ -6,37 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-recipe.component.css']
 })
 export class NewRecipeComponent implements OnInit {
-  ingredient = [];
-  newIngredient = '';
+
+  ingredients: Ingredient[] = [];
+
+  newIngredientName = '';
+  newIngredientAmount = 1;
+  sameIngredientCounter = 0;
+
   errorMsg = '';
   errorStatus = false;
 
   constructor() { }
-
   ngOnInit() {
   }
 
   addIngredient() {
-      if(this.newIngredient === '') {
-        this.errorMsg = 'Nie podałeś składnika!!!';
+    if (this.newIngredientName === '') {
+      this.errorMsg = 'Musisz podać nazwę składnika';
+      this.errorStatus = true;
       } else {
-        if(!this.ingredient.includes(this.newIngredient)) {
-          this.ingredient.push(this.newIngredient);
-          this.newIngredient='';
-          this.errorMsg = '';
+      if (this.ingredients.length > 0) {
+        this.ingredients.forEach(element => {
+          if (element.name === this.newIngredientName) {
+            this.sameIngredientCounter++;
+          }
+        });
+        if(this.sameIngredientCounter > 0) {
+          this.errorMsg = this.newIngredientName + ' już się znajduje na liście składników.'
+          this.errorStatus = true;
+          this.newIngredientName = '';
+          this.sameIngredientCounter = 0;
         } else {
-          this.errorMsg = 'Sładnik "'+ this.newIngredient + '" już jest na liście';
-
+          this.errorMsg = '';
+          this.errorStatus = false;
         }
       }
-      if(this.errorMsg === '') {
-        this.errorStatus = false;
-      } else {
-        this.errorStatus = true;
-      }
+      if (this.errorStatus === false) {
+        this.ingredients.push({ "name": this.newIngredientName, "amount": this.newIngredientAmount });
+      this.newIngredientName = '';
+      this.newIngredientAmount = 1;
+      this.errorStatus = false;
     }
+
+    }
+        }
   
   deleteIngredient(index: number) {
-    this.ingredient.splice(index, 1);
+    this.ingredients.splice(index, 1);
   }
 }
