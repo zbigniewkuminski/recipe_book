a5deaf06348277785cc3685c65517dacb829b965
Zbigniew Kumiński <zbigniew.kuminski@gmail.com>
2018-07-11 13:47:47 +0200

    service data storage

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,26 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../shared/recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { AddRecipeService } from '../service/add-recipe.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [AddRecipeService]
 })
 export class ListComponent implements OnInit {
-  recipes: Recipe[] = [ 
-    new Recipe('kanapka',[{name: "chleb", amount: 2},{name: "masło", amount: 1}],'./assets/images/kanapka.jpg','Pokrój chleb. Posmaruj masłem.'),
-    new Recipe('jajecznica',[{name: "jajka", amount: 4},{name: "chleb", amount: 2}],'../assets/images/jajecznica.jpg','Rozgrzej patelnię. Wbij jajka.'),    
-    new Recipe('ryż',[{name: "ryż", amount: 1}],'../assets/images/ryż.jpg','Ugotuj ryż.'),  
-  ];
+  recipes: Recipe[] = [];
   @Input() featureTitle: string;
 
-  constructor() { }
+  constructor(private addRecipe: AddRecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.addRecipe.recipes;
   }
 
-  // addNewRecipe(recipe: {name: string, description: string, ingredients: Ingredient[], imgPath: string}) {
-  //   this.recipes.push(new Recipe(recipe.name, recipe.ingredients, recipe.imgPath, recipe.description));
-  // }
 }
