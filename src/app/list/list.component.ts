b9b709f9e5e524aad1d17db0a4c499ae02bdd424
Zbigniew Kumiński <zbigniew.kuminski@gmail.com>
2018-07-18 13:19:08 +0200

    pagination

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,18 +7,50 @@ import { AddRecipeService } from '../service/add-recipe.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
+
 export class ListComponent implements OnInit {
   recipes: Recipe[] = [];
+  recipePagination: Recipe[];
+
+  recipesOnPage = 3;
+  page = 1;
+
+  numberOfPages = 1;
 
   @Input() featureTitle: string;
-
-  constructor(private recipeService: AddRecipeService){}
+  constructor(private recipeService: AddRecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.recipesArray;
+    this.numberOfPages = this.recipes.length / this.recipesOnPage;
+    this.recipePagination = [];
+    this.loadedRecipes();
   }
 
   deleteRecipe(id: number) {
     this.recipeService.deleteRecipe(id);
   }
+
+  prevPage() {
+    this.page--;
+    this.recipePagination = [];
+    this.loadedRecipes();
+  }
+
+  nextPage() {
+    this.page++;
+    this.recipePagination = [];
+    this.loadedRecipes();
+  }
+
+  loadedRecipes() {
+    this.recipes.forEach((recipe, index) => {
+      if ((index >= ((this.page - 1) * this.recipesOnPage)) &&
+        (index <= (this.page * this.recipesOnPage) - 1)){
+          this.recipePagination.push(recipe);
+        }
+    });
+    console.log(this.recipePagination);
+  }
+
 }
